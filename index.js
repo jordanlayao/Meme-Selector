ac748d0053acefa68b0e6c844593660ef071a46b
Jordan Layao <58056065+jordanlayao@users.noreply.github.com>
2025-07-13 09:59:06 -0700

    Commit main files

---
+++ index.js
@@ -0,0 +1,19 @@
+import { catsData } from '/data.js'
+
+
+function getEmotionsArray(cats){
+    const emotionsArray = []    
+    for (let cat of cats){
+        for (let emotion of cat.emotionTags){
+            if (!emotionsArray.includes(emotion)){
+                emotionsArray.push(emotion)
+            }
+        }
+    }
+    return emotionsArray
+}
+
+
+
+
+
