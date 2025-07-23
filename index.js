be4d6de8c5044f38f7605ac5e771388cd941f591
Jordan Layao <58056065+jordanlayao@users.noreply.github.com>
2025-07-23 10:20:52 -0400

    Update

--- index.js
+++ index.js
@@ -1,28 +1,70 @@
 import { catsData } from '/data.js'
 
-const emotionsRadios = document.getElementById("emotion-radios")
-const imageButton = document.getElementById("get-image-btn")
-const gifOnly = document.getElementById("gifs-only-option")
+const emotionRadios = document.getElementById('emotion-radios')
+const getImageBtn = document.getElementById('get-image-btn')
+const gifsOnlyOption = document.getElementById('gifs-only-option')
+const memeModalInner = document.getElementById('meme-modal-inner')
+const memeModal = document.getElementById('meme-modal')
+const memeModalCloseBtn = document.getElementById('meme-modal-close-btn')
 
-imageButton.addEventListener('click', getMatchingCatsArray)
+emotionRadios.addEventListener('change', highlightCheckedOption)
 
-function getMatchingCatsArray(){
-    const isGif = gifOnly.checked
-    console.log(isGif)
-    if(document.querySelector('input[type="radio"]:checked')) {
-        const option = document.querySelector('input[type="radio"]:checked').value
-        console.log(option)
+memeModalCloseBtn.addEventListener('click', closeModal)
+
+getImageBtn.addEventListener('click', renderCat)
+
+function highlightCheckedOption(e){
+    const radios = document.getElementsByClassName('radio')
+    for (let radio of radios){
+        radio.classList.remove('highlight')
+    }
+    document.getElementById(e.target.id).parentElement.classList.add('highlight')
+}
+
+function closeModal(){
+    memeModal.style.display = 'none'
+}
+
+function renderCat(){
+    const catObject = getSingleCatObject()
+    memeModalInner.innerHTML =  `
+        <img 
+        class="cat-img" 
+        src="./images/${catObject.image}"
+        alt="${catObject.alt}"
+        >
+        `
+    memeModal.style.display = 'flex'
+}
+
+function getSingleCatObject(){
+    const catsArray = getMatchingCatsArray()
+    
+    if(catsArray.length === 1){
+        return catsArray[0]
+    }
+    else{
+        const randomNumber = Math.floor(Math.random() * catsArray.length)
+        return catsArray[randomNumber]
     }
 }
 
-emotionsRadios.addEventListener('change', highlightCheckedOption)
-
-function highlightCheckedOption(e){
-    const radioArray = document.getElementsByClassName('radio')
-    for (let radio of radioArray) {
-        radio.classList.remove('highlight')
-    }
-    e.target.parentElement.classList.add('highlight')
+function getMatchingCatsArray(){     
+    if(document.querySelector('input[type="radio"]:checked')){
+        const selectedEmotion = document.querySelector('input[type="radio"]:checked').value
+        const isGif = gifsOnlyOption.checked
+        
+        const matchingCatsArray = catsData.filter(function(cat){
+            
+            if(isGif){
+                return cat.emotionTags.includes(selectedEmotion) && cat.isGif
+            }
+            else{
+                return cat.emotionTags.includes(selectedEmotion)
+            }            
+        })
+        return matchingCatsArray 
+    }  
 }
 
 function getEmotionsArray(cats){
@@ -37,26 +79,23 @@ function getEmotionsArray(cats){
     return emotionsArray
 }
 
-function renderEmotionRadios(x){
+function renderEmotionsRadios(cats){
+        
     let radioItems = ``
-    const emotions = getEmotionsArray(x)
+    const emotions = getEmotionsArray(cats)
     for (let emotion of emotions){
         radioItems += `
-            <div class="radio">
-                <label for="${emotion}">${emotion}</label>
-                <input
-                type = "radio"
-                id = "${emotion}"
-                value = "${emotion}"
-                name = "emotion"
-                >
-            </div>
-            `
+        <div class="radio">
+            <label for="${emotion}">${emotion}</label>
+            <input
+            type="radio"
+            id="${emotion}"
+            value="${emotion}"
+            name="emotions"
+            >
+        </div>`
     }
-    emotionsRadios.innerHTML = radioItems
+    emotionRadios.innerHTML = radioItems
 }
 
-renderEmotionRadios(catsData)
-
-
-
+renderEmotionsRadios(catsData)
